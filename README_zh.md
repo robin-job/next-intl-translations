0e628b08e89af05f192d95e755dfac9ad41101e8
robin-job <120911231+robin-job@users.noreply.github.com>
2025-04-05 15:22:53 +0300

    updated build scripts and tsconfig, wrote new README.md, updated extract-translations

---
+++ README_zh.md
@@ -0,0 +1,1 @@
+[README in English](README) | [README in Russian](README_ru)
--- scripts/extract-translations.js
+++ scripts/extract-translations.js
@@ -1,81 +1,92 @@
-#!/usr/bin/env node
-import fs from "fs";
+import * as dotenv from "dotenv";
+import { writeFileSync, existsSync } from "fs";
 import path from "path";
 
-const TRANSLATIONS_DIR = process.argv[2] || "./messages";
-const EXTRACTION_PATH = process.argv[3] || "src";
-const TRANSLATIONS_FILE_NAME = process.argv[4] || "translations";
-const LOG_OUTPUT = process.argv[5] === "false" ? false : true;
+dotenv.config();
+
+import {
+  formatObject,
+  readLocales,
+  ensureDirectoryExists,
+} from "./translation-utils.mjs";
+
+const {
+  NIT_MESSAGES_DIR = "./messages",
+  NIT_EXTRACTION_PATH = "src",
+  NIT_TRANSLATIONS_FILE_NAME = "translations",
+} = process.env;
+
+const NIT_LOG_OUTPUT = process.env.NIT_LOG_OUTPUT === "false" ? false : true;
+const NIT_NEW_TRANSLATIONS_FILE =
+  process.env.NIT_NEW_TRANSLATIONS_FILE === "true" ? true : false;
 
 async function processTranslations() {
   try {
-    const localeFiles = fs.readdirSync(TRANSLATIONS_DIR);
-    const locales = localeFiles
-      .filter((file) => file.endsWith(".json"))
-      .map((file) => file.replace(".json", ""));
+    const translations = readLocales(NIT_MESSAGES_DIR);
 
-    if (locales.length === 0) {
+    if (Object.keys(translations).length === 0) {
       console.warn("No locale files found in the messages directory.");
       return;
     }
 
-    const translations = {};
-    for (const locale of locales) {
-      const filePath = path.join(TRANSLATIONS_DIR, `${locale}.json`);
-      const fileContent = fs.readFileSync(filePath, "utf-8");
-      translations[locale] = JSON.parse(fileContent);
-    }
+    const locales = Object.keys(translations);
 
-    for (const section in translations[locales[0]]) {
-      const sectionData = translations[locales[0]][section];
-      for (const component in sectionData) {
-        const componentName = component;
+    async function traverseTranslations(obj, currentPath) {
+      for (const key in obj) {
+        if (typeof obj[key] === "object" && obj[key] !== null) {
+          await traverseTranslations(obj[key], path.join(currentPath, key));
+        }
+      }
 
-        // Ищем файл translations.ts, translations.json или translations.js
-        const translationsFilePath = await findTranslationsFile(
-          path.join(EXTRACTION_PATH),
-          componentName
-        );
+      if (
+        !Object.values(obj).some(
+          (value) => typeof value === "object" && value !== null
+        )
+      ) {
+        const dirPath = path.join(NIT_EXTRACTION_PATH, currentPath);
+        const filePath = path.join(dirPath, `${NIT_TRANSLATIONS_FILE_NAME}.ts`);
 
-        if (translationsFilePath) {
-          const translationsForComponent = {};
-          for (const locale of locales) {
-            translationsForComponent[locale] =
-              translations[locale][section][componentName];
-          }
+        ensureDirectoryExists(dirPath);
 
-          // Определяем тип файла (ts, json или js) и вызываем соответствующую функцию
-          if (translationsFilePath.endsWith(".ts")) {
-            await updateTranslationsFileTs(
-              translationsFilePath,
-              translationsForComponent
-            );
-          } else if (translationsFilePath.endsWith(".json")) {
-            await updateTranslationsFileJson(
-              translationsFilePath,
-              translationsForComponent
-            );
-          } else if (translationsFilePath.endsWith(".js")) {
-            await updateTranslationsFileJs(
-              translationsFilePath,
-              translationsForComponent
-            );
-          }
+        const translationsForComponent = {};
+        for (const locale of locales) {
+          translationsForComponent[locale] = getTranslationsForPath(
+            translations[locale],
+            currentPath.split(path.sep)
+          );
+        }
 
-          if (LOG_OUTPUT) {
-            console.log(
-              `Updated translations for ${componentName} in ${translationsFilePath}`
-            );
+        if (NIT_NEW_TRANSLATIONS_FILE) {
+          await createTranslationsFileTs(filePath, translationsForComponent);
+          if (NIT_LOG_OUTPUT) {
+            console.log(`Created translations file in ${filePath}`);
           }
         } else {
-          console.warn(
-            `Файл translations.ts, translations.json или translations.js не существует для ${componentName} в директории ${EXTRACTION_PATH}.`
-          );
+          if (existsSync(filePath)) {
+            await updateTranslationsFileTs(filePath, translationsForComponent);
+            if (NIT_LOG_OUTPUT) {
+              console.log(`Updated translations file in ${filePath}`);
+            }
+          } else {
+            console.warn(`File ${filePath} not found, skipping`);
+          }
         }
       }
     }
 
-    if (LOG_OUTPUT) {
+    function getTranslationsForPath(translation, pathParts) {
+      let current = translation;
+      for (const part of pathParts) {
+        current = current[part];
+      }
+      return current;
+    }
+
+    for (const key in translations[locales[0]]) {
+      await traverseTranslations(translations[locales[0]][key], key);
+    }
+
+    if (NIT_LOG_OUTPUT) {
       console.log("Translations processed successfully!");
     }
   } catch (error) {
@@ -83,49 +94,43 @@ async function processTranslations() {
   }
 }
 
-async function findTranslationsFile(dir, componentName) {
-  const files = fs.readdirSync(dir);
+async function createTranslationsFileTs(filePath, translations) {
+  try {
+    // Получаем список локалей
+    const locales = Object.keys(translations);
 
-  for (const file of files) {
-    const filePath = path.join(dir, file);
-    const stat = fs.statSync(filePath);
+    // Выбираем первую локаль (например, 'en') для определения типа
+    const firstLocale = locales[0];
+    const typeDefinition = `typeof ${firstLocale}`;
 
-    if (stat.isDirectory()) {
-      const foundPath = await findTranslationsFile(filePath, componentName);
-      if (foundPath) {
-        return foundPath;
-      }
-    } else if (
-      (file === `${TRANSLATIONS_FILE_NAME}.ts` ||
-        file === `${TRANSLATIONS_FILE_NAME}.json` ||
-        file === `${TRANSLATIONS_FILE_NAME}.js`) &&
-      filePath.includes(componentName)
-    ) {
-      return filePath;
-    }
+    // Создаем строки для каждой локали
+    // Создаем строки для каждой локали
+    const localeStrings = locales
+      .map((locale) => {
+        const data = translations[locale];
+        const formattedData = formatObject(data);
+        if (locale === firstLocale) {
+          return `const ${locale} = ${formattedData};`;
+        } else {
+          return `const ${locale}: ${typeDefinition} = ${formattedData};`;
+        }
+      })
+      .join("\n\n");
+
+    // Создаем строку для export default
+    const satisfiesType = "TLocales"; // Замените на ваш тип
+    const exportDefault = `\nexport default { ${locales.join(
+      ", "
+    )} } satisfies ${satisfiesType};`;
+
+    // Создаем полный контент файла
+    let newContent = localeStrings + "\n";
+    newContent += exportDefault + "\n";
+
+    writeFileSync(filePath, newContent, "utf-8");
+  } catch (error) {
+    console.error(`Error creating translations TS file: ${filePath}`, error);
   }
-
-  return null;
-}
-
-function formatObject(obj, indent = 0) {
-  const indentStr = "  ".repeat(indent);
-  const newIndentStr = "  ".repeat(indent + 1);
-
-  if (typeof obj !== "object" || obj === null) {
-    return `'${obj}'`;
-  }
-
-  const keys = Object.keys(obj);
-  const items = keys.map((key) => {
-    const value = obj[key];
-    if (typeof value === "object" && value !== null) {
-      return `${newIndentStr}${key}: ${formatObject(value, indent + 1)}`;
-    }
-    return `${newIndentStr}${key}: '${value}'`;
-  });
-
-  return `{\n${items.join(",\n")},\n${indentStr}}`;
 }
 
 async function updateTranslationsFileTs(filePath, translations) {
@@ -151,50 +156,18 @@ async function updateTranslationsFileTs(filePath, translations) {
       .join("\n\n");
 
     // Создаем строку для export default
-    const exportDefault = `export default { ${locales.join(
+    const satisfiesType = "TLocales"; // Замените на ваш тип
+    const exportDefault = `\nexport default { ${locales.join(
       ", "
-    )} } satisfies TLocales;`;
+    )} } satisfies ${satisfiesType};`;
 
     // Создаем полный контент файла
-    let newContent = localeStrings + "\n\n";
+    let newContent = localeStrings + "\n";
     newContent += exportDefault + "\n";
 
-    fs.writeFileSync(filePath, newContent, "utf-8");
+    writeFileSync(filePath, newContent, "utf-8");
   } catch (error) {
     console.error(`Error updating translations TS file: ${filePath}`, error);
-    throw error;
-  }
-}
-
-async function updateTranslationsFileJson(filePath, translations) {
-  try {
-    const jsonData = {};
-    for (const locale in translations) {
-      jsonData[locale] = translations[locale];
-    }
-
-    const jsonString = JSON.stringify(jsonData, null, 2);
-    fs.writeFileSync(filePath, jsonString + "\n", "utf-8");
-  } catch (error) {
-    console.error(`Error updating translations JSON file: ${filePath}`, error);
-    throw error;
-  }
-}
-
-async function updateTranslationsFileJs(filePath, translations) {
-  try {
-    const localesObject = {};
-    for (const locale in translations) {
-      localesObject[locale] = translations[locale];
-    }
-
-    const objectString = formatObject(localesObject);
-    const jsContent = `const locales = ${objectString};\n\nexport default locales;\n`;
-
-    fs.writeFileSync(filePath, jsContent, "utf-8");
-  } catch (error) {
-    console.error(`Error updating translations JS file: ${filePath}`, error);
-    throw error;
   }
 }
 
